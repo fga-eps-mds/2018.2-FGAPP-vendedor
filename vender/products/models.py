ed3f797c7776b0b60ce3ec22b9ee7de30b87d7b6
Welison Lucas <welison.almeida.923@gmail.com>
2018-09-05 21:31:30 -0300

    Creation of Product model and corresponding serializer.
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>
    Co-authored-by: Andre Lucas <andrelucax@gmail.com>

--- vender/products/models.py
+++ vender/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    created = models.DateTimeField(auto_now_add= True)
+    title = models.CharField(max_length = 50, blank = False)
+    description = models.TextField()
+    price = models.DecimalField(max_digits= 5, decimal_places= 2, default= '000.00')
+
+    class Meta:
+        ordering = ('created',)
