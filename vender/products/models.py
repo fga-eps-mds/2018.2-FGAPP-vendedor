79bc67facb7cf2590aab85ce6e7416b75c2aefd5
Andre Lucas <andrelucax@gmail.com>
2018-09-07 19:00:42 -0300

    Quantity and owner with authentication added to product class.
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>
    Co-authored-by: Andre Lucas <andrelucax@gmail.com>

--- vender/products/models.py
+++ vender/products/models.py
@@ -6,6 +6,8 @@ class Product(models.Model):
     title = models.CharField(max_length = 50, blank = False)
     description = models.TextField()
     price = models.DecimalField(max_digits= 5, decimal_places= 2, default= '000.00')
+    quantity = models.IntegerField(default= '00')
+    owner = models.ForeignKey('auth.User', related_name='products', on_delete=models.CASCADE)
 
     class Meta:
         ordering = ('created',)
--- vender/products/serializers.py
+++ vender/products/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from products.models import Product
+from django.contrib.auth.models import User
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ('id', 'created', 'title', 'description', 'price')
+        fields = ('id', 'created', 'title', 'description', 'price', 'quantity')
+        owner = serializers.ReadOnlyField(source='owner.username')
+
+class UserSerializer(serializers.ModelSerializer):
+    products = serializers.PrimaryKeyRelatedField(many=True, queryset=Product.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'products')
