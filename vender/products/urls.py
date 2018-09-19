3e815b69a88796ecda4d7a83052eec74cf10e333
Andre Lucas <andrelucax@gmail.com>
2018-09-18 23:13:25 -0300

    Creating a function to delete a product
    Co-authored-by: André Lucas de Sousa Pinto <andrelucax@gmail.com>
    Co-authored-by: Leonardo Medeiros  <leonardomedeiros.6@gmail.com>

--- vender/products/urls.py
+++ vender/products/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from products import views
 from django.conf.urls import include
+from .views import ProductDelete
+from django.urls import path
 
 urlpatterns = [
     url(r'^products/$', views.ProductList.as_view()),
@@ -10,4 +12,6 @@ urlpatterns = [
 
     url(r'^users/$', views.UserList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+
+    path('api/product_delete', ProductDelete),
 ]
--- vender/products/views.py
+++ vender/products/views.py
@@ -5,7 +5,9 @@ from django.contrib.auth.models import User
 from products.serializers import UserSerializer
 from rest_framework import permissions
 from products.permissions import IsOwnerOrReadOnly
-
+from django.test import Client
+from rest_framework.test import APIClient
+from rest_framework.decorators import api_view
 
 class ProductList(generics.ListCreateAPIView):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
@@ -29,3 +31,13 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+@api_view(["POST"])
+def ProductDelete(request):
+    client = APIClient()
+    client.login(username=request.data.get("email"),password=request.data.get("password"))
+
+    path = '/products/' + request.data.get("product_id") + '/'
+    response = client.delete(path)
+
+    return response
